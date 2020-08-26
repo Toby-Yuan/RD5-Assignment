b6c42916c7beed179c131c205ff009038b3f6b7d
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 16:54:26 +0800

    member : 初切+樣式1

---
+++ member.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網路銀行</title>
+    <link rel="stylesheet" href="CSS/memberStyle.css">
+</head>
+<body>
+    <div id="contant">
+        <h1>HELLO user !!</h1>
+        <div id="box">
+            <a href="" class="act">
+                存款
+            </a>
+            <a href="" class="act">
+                提款    
+            </a>
+            <a href="" class="act">
+                查詢餘額
+            </a>
+        </div>
+    </div>
+</body>
+</html>
