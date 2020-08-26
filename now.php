6850aa51e4539c42786651b7322fbea8f3714312
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 17:23:56 +0800

    新增結算畫面以及樣式

---
+++ now.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網路銀行</title>
+    <link rel="stylesheet" href="CSS/nowStyle.css">
+</head>
+<body>
+    <div id="box">
+        <h1>本次紀錄</h1>
+        <h2>存款</h2>
+        <p>本次交易金額:&nbsp;<span>1000</span></p>
+        <p>帳戶餘額:&nbsp;<span>3000</span></p>
+        <div></div>
+        <a href="" id="link">回首頁</a>
+    </div>
+</body>
+</html>
