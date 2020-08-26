411c2c873b5084b3d970782e51951a113dae567c
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 17:11:27 +0800

    新增存款和提款頁面以及樣式

---
+++ deposit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網路銀行</title>
+    <link rel="stylesheet" href="CSS/depositStyle.css">
+</head>
+<body>
+    <form action="" method="post">
+        <div id="fast">
+            <h1>快速提款</h1>
+            <input type="submit" value="1000">
+            <input type="submit" value="3000">
+            <input type="submit" value="5000">
+            <input type="submit" value="7000">
+            <input type="submit" value="10000">
+        </div>
+        <div id="typeIt">
+            <h1>輸入金額</h1>
+            <input type="text" name="money" id="money">
+            <input type="submit" value="送出" id="submit">
+            <input type="submit" value="回首頁" id="back">
+        </div>
+    </form>
+</body>
+</html>
