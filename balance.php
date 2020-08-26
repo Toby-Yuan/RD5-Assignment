76c654e684d796dff2dc535d2ac87884311d60c9
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 17:38:41 +0800

    新增餘額畫面以及樣式檔

---
+++ balance.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網路銀行</title>
+    <link rel="stylesheet" href="CSS/balanceStyle.css">
+</head>
+<body>
+    <div id="box">
+        <h1>現有餘額</h1>
+        <p>3000</p>
+        <a href="">回首頁</a>
+
+        <h2>交易明細</h2>
+        <table>
+            <tr>
+                <th>時間戳記</th>
+                <th>存款</th>
+                <th>提款</th>
+                <th>餘額</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
