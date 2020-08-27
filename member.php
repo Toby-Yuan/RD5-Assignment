76bd0c85fddd5e160408bef6bbc19975bac3fe12
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-27 14:25:33 +0800

    member : 抓取登入者資料

--- member.php
+++ member.php
@@ -1,3 +1,20 @@
+<?php
+
+session_start();
+require_once("connect.php");
+
+if(!isset($_SESSION["uid"])){
+    header("location: index.php");
+    exit();
+}else{
+    $uid = $_SESSION["uid"];
+    $search = "SELECT userName FROM member WHERE id = $uid";
+    $result = mysqli_query($link, $search);
+    $row = mysqli_fetch_assoc($result);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="zh">
 <head>
@@ -8,18 +25,20 @@
 </head>
 <body>
     <div id="contant">
-        <h1>HELLO user !!</h1>
+        <h1>HELLO <?= $row["userName"] ?> !!</h1>
         <div id="box">
-            <a href="" class="act">
+            <a href="deposit.php" class="act">
                 存款
             </a>
-            <a href="" class="act">
+            <a href="withdrawal.php" class="act">
                 提款    
             </a>
-            <a href="" class="act">
+            <a href="balance" class="act">
                 查詢餘額
             </a>
         </div>
+
+        <a href="index.php?logout=1">登出</a>
     </div>
 </body>
 </html>
