d687f0a3a72fb776247cedcb4292f733dc7109c1
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-31 17:47:19 +0800

    新增匯款頁面

---
+++ transfer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網路銀行</title>
+    <link rel="stylesheet" href="CSS/transferStyle.css">
+</head>
+<body>
+    <div id="box">
+        <h1>匯款資訊</h1>
+
+        <form action="" method="post">
+            <label for="tranName">受匯人帳戶</label>
+            <input type="text" id="tranName" name="tranName">
+            <label for="tranMoney">匯款金額</label>
+            <input type="text" id="tranMoney" name="tranMoney">
+
+            <input type="submit" value="送出" name="submit">
+            <input type="submit" value="回首頁" name="back">
+        </form>
+    </div>
+</body>
+</html>
